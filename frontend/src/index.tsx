63370ecaeafb4b0b9c75f763c562e061a1af2fef
Samuel Mota <smagnun@gmail.com>
2021-05-08 15:29:40 +0200

    bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/css/styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
